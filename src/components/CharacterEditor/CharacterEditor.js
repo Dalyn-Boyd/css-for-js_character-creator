5d8dfe4baccd92441edb881202cd399e664a89e0
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-01-24 19:29:47 -0600

    added perspective effect

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -1,20 +1,17 @@
 import React from 'react';
 
-import {
-  defaultSkinColor,
-  defaultClothesColor,
-} from '../../constants';
+import { defaultClothesColor, defaultSkinColor } from '../../constants';
 import Character from '../Character';
-import MaxWidthWrapper from '../MaxWidthWrapper';
 import ControlPane from '../ControlPane';
+import MaxWidthWrapper from '../MaxWidthWrapper';
 
 import {
-  bodyOptions,
-  headOptions,
-  faceOptions,
   accessoryOptions,
-  skinColorOptions,
+  bodyOptions,
   clothesColorOptions,
+  faceOptions,
+  headOptions,
+  skinColorOptions,
 } from './CharacterEditor.helpers';
 import styles from './CharacterEditor.module.css';
 
@@ -24,53 +21,37 @@ function App() {
   const [face, setFace] = React.useState(0);
   const [accessory, setAccessory] = React.useState(0);
   const [skinColor, setSkinColor] = React.useState(defaultSkinColor);
-  const [clothesColor, setClothesColor] = React.useState(
-    defaultClothesColor
-  );
+  const [clothesColor, setClothesColor] = React.useState(defaultClothesColor);
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspectiveEffect}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
-            Customize your character's look and style using the
-            controls below. What sort of adventure will you embark on?{' '}
+            Customize your character's look and style using the controls below. What sort of adventure will you embark
+            on?{' '}
           </p>
         </header>
         <div className={styles.controlColumn}>
+          <ControlPane title='Bodies' options={bodyOptions} currentOption={body} handleSelectOption={setBody} />
+          <ControlPane title='Heads' options={headOptions} currentOption={head} handleSelectOption={setHead} />
+          <ControlPane title='Faces' options={faceOptions} currentOption={face} handleSelectOption={setFace} />
           <ControlPane
-            title="Bodies"
-            options={bodyOptions}
-            currentOption={body}
-            handleSelectOption={setBody}
-          />
-          <ControlPane
-            title="Heads"
-            options={headOptions}
-            currentOption={head}
-            handleSelectOption={setHead}
-          />
-          <ControlPane
-            title="Faces"
-            options={faceOptions}
-            currentOption={face}
-            handleSelectOption={setFace}
-          />
-          <ControlPane
-            title="Accessories"
+            title='Accessories'
             options={accessoryOptions}
             currentOption={accessory}
             handleSelectOption={setAccessory}
           />
           <ControlPane
-            title="Skin Color"
+            title='Skin Color'
             options={skinColorOptions}
             currentOption={skinColor}
             handleSelectOption={setSkinColor}
           />
           <ControlPane
-            title="Clothing Color"
+            title='Clothing Color'
             options={clothesColorOptions}
             currentOption={clothesColor}
             handleSelectOption={setClothesColor}
